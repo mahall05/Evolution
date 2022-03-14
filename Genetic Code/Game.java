0d8799a85e3b4b0dcfbed9709a272f81036427d9
Matt <hallmatthewd12@gmail.com>
2022-03-14 15:56:06 -0400

    Setup Background - Created a Window on Screen

---
+++ Genetic Code/Game.java
@@ -0,0 +1,161 @@
+import java.util.Random;
+import javax.swing.JFrame;
+import java.awt.*;
+import java.awt.image.BufferStrategy;
+
+public class Game extends Canvas implements Runnable{
+    public static final int WIDTH = 640, HEIGHT = WIDTH / 12 * 9;     // Set the size of the window
+
+    private Thread thread;  // The game runs on this thread
+    private boolean running = false;
+
+    public static boolean paused = false;
+    //public int diff;
+    
+    // 0 = normal
+    // 1 = hard
+
+    //private HUD hud;
+
+    private Random r;
+
+    
+    public enum STATE{
+        Menu,
+        Select,
+        Help,
+        Shop,
+        Game,
+        End
+    };
+    
+
+    public static STATE gameState = STATE.Game;
+
+    public Game(){
+        //hud = new HUD();
+        new Window(WIDTH, HEIGHT, "Let's Build a Game!", this);
+
+        r = new Random();
+    }
+
+    public synchronized void start(){
+        thread = new Thread(this);     // Initialize a new thread
+		thread.start();     // Start the thread
+		running = true;     // Thread is running
+    }
+
+    public synchronized void stop(){
+        try{
+            thread.join();  // Stops the thread
+            running = false;  // Thread is not running
+        }catch(Exception e){
+            e.printStackTrace();  // Print an error message
+        }
+    }
+
+    public void run() {     // The game loop
+		this.requestFocus();
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;     // The amount of ticks/second
+		double ns = 1000000000 / amountOfTicks;     // The amount of nanoticks/tick
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int frames = 0;
+		while(running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while(delta >= 1) {
+				tick();
+				delta--;
+			}
+			if(running)
+				render();
+			frames++;
+			
+			if(System.currentTimeMillis() - timer > 1000) {
+				timer += 1000;
+				//System.out.println("FPS: " + frames);
+				frames = 0;
+			}
+		}
+		stop();
+	}
+
+    private void tick(){
+        if(gameState == STATE.Game){
+            if(!paused){
+                //hud.tick();
+                /*
+				spawner.tick();
+				handler.tick();
+				if(HUD.HEALTH <= 0) {
+					HUD.HEALTH = 100;
+					gameState = STATE.End;
+					handler.clearEnemies();
+					for(int i = 0; i < 20; i++) {
+						handler.addObject(new MenuParticle(r.nextInt(WIDTH - 16), r.nextInt(HEIGHT - 16), ID.MenuParticle, handler));
+					}
+				}
+                */
+            }
+        }
+    }
+
+    private void render(){
+        BufferStrategy bs = this.getBufferStrategy();
+        if(bs == null){
+            this.createBufferStrategy(3);
+            return;
+        }
+
+        Graphics g = bs.getDrawGraphics();
+
+        g.setColor(Color.black);
+        g.fillRect(0, 0, WIDTH, HEIGHT);
+
+        if(paused){
+            g.setColor(Color.white);
+            g.drawString("PAUSED", 100, 100);
+        }
+
+        if(gameState == STATE.Game){
+            //hud.render(g);
+            //handler.render(g);
+        }/*else if(gameState == STATE.Shop) {
+			shop.render(g);
+		}else if(gameState == STATE.Menu || gameState == STATE.Help || gameState == STATE.End || gameState == STATE.Select) {
+			menu.render(g);
+			handler.render(g);
+		}*/
+
+        g.dispose();
+        bs.show();
+    }
+
+    public static float clamp(float var, float min, int max){
+        if(var >= max)
+			return var = max;
+		else if(var <= min)
+			return var = min;
+		else
+			return var;
+    }
+
+    public static int randomInt(int minInclusive, int maxInclusive){
+        int range = maxInclusive - minInclusive + 1;
+        int rng = (int)(Math.random() * range) + minInclusive;
+        return rng;
+    }
+
+    public static double randomDouble(double minInclusive, double maxInclusive){
+        double range = maxInclusive - minInclusive + 1;
+        double rng = (Math.random() * range) + minInclusive;
+        return rng;
+    }
+	
+	public static void main(String args[]) {
+		new Game();
+	}
+}
