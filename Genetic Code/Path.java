c5549335637b1ac2383ff7a29b276f02f133b2ea
Matt <hallmatthewd12@gmail.com>
2022-03-15 16:53:36 -0400

    Added a new TODO
    Will hopefully work on this at robotics

--- Genetic Code/Path.java
+++ Genetic Code/Path.java
@@ -24,13 +24,16 @@ public class Path{
   public void accelerate(AccelVector a){
     xVel += a.xAcc;
     yVel += a.yAcc;
+    // TODO figure out a way to keep the angle of movement the same, but reduce the length of movement
     while(calcVel() > velLimit){
+      if(xVel < 0 && yVel < 0){
+        xVel += 1;
+        yVel += 1;
+      }
+      else if(xVel > 0 && xVel > 0){
       xVel -= 1;
       yVel -= 1;
-    }
-    while(calcVel() < -velLimit){
-      xVel += 1;
-      yVel += 1;
+      }
     }
   }
 
---
+++ Genetic Code/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+    public static void main(String[] args){
+        int x = 7;
+        int y = 7;
+        //System.out.println(Math.sqrt())
+    }
+}
