cdd6baf5d1c60aa33db8d36ecb84cc6a16ba416d
Matt <hallmatthewd12@gmail.com>
2022-04-28 10:05:18 -0400

    I fixed it. I'm just an idiot.
    The info itself wasn't null, it was the date within the info. I even set it to null myself, I'm just stupid.

--- Genetic Code/Menus/FileMenu.java
+++ Genetic Code/Menus/FileMenu.java
@@ -14,7 +14,7 @@ public class FileMenu {
     public FileMenu(){
         for(int i = 0; i < buttons.length; i++){
             if(i == 0){
-                buttons[i] = new Button(Constants.WIDTH/2-100, Constants.HEIGHT/2-160, 200, 75); // Create the first buttons
+                buttons[i] = new Button(Constants.WIDTH/2-200, Constants.HEIGHT/2-160, 200, 75); // Create the first buttons
                 buttons[i].bold = true;
             }else{
                 buttons[i] = buttons[i-1].copy(buttons[i-1].x, buttons[i-1].y+buttons[i-1].height+20); // Other buttons are copies of the first in different positions
@@ -26,6 +26,10 @@ public class FileMenu {
         // Perform load operation
         load();
         load();
+
+        for(int i = 0; i < buttons.length; i++){
+            System.out.println(info[i].ableToReachGoal);
+        }
     }
 
     public void tick(){
@@ -43,12 +47,13 @@ public class FileMenu {
             buttons[i].render(g);
         }
 
+        g.setFont(new Font(g.getFont().getFontName(), Font.PLAIN, 15));
+        g.setColor(Color.white);
+
         for(int i = 0; i < buttons.length; i++){
-            if(!info[i].equals(null)){
-                g.drawString(info[i].date.toString(), buttons[i].x+100, buttons[i].y+10);
-            }else{
-                g.drawString("Null", buttons[i].x+100, buttons[i].y+10);
-            }
+            g.drawString(info[i].date==null ? "Empty" : info[i].date.toString(), buttons[i].x+225, buttons[i].y+15);
+            g.drawString("Generation: " + info[i].generation, buttons[i].x+225, buttons[i].y+30);
+            g.drawString(info[i].ableToReachGoal ? "Able to reach goal in " + info[i].steps + " steps": "Not able to reach goal", buttons[i].x+225, buttons[i].y+45);
         }
     }
 
